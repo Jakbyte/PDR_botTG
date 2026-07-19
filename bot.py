31264e68721463224d136ce7bc7250f49188725c
Jakbyte <jakbyte@gmail.com>
2026-07-19 13:43:20 +0300

    upd

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 from create_bot import dp
-from handlers import user_hd
+from handlers import user_hd, admin_hd
 
 def register_all_handlers():
     dp.include_router(user_hd.router)
+    dp.include_router(admin_hd.router)
--- main.py
+++ main.py
@@ -1,39 +1,31 @@
 import logging
 import asyncio
-import os 
-
-from aiogram import Bot, Dispatcher
 from aiogram.types import BotCommand
-from handlers import user_hd, admin_hd
-from dotenv import load_dotenv
 
+from create_bot import bot, dp
+from bot import register_all_handlers
 from db_users.connect import init_users_db
 
 
-load_dotenv()
+register_all_handlers()
 
-bot = Bot(token=os.getenv('BOT_TOKEN'))
-dp = Dispatcher()
-
-dp.include_router(admin_hd.router)
-dp.include_router(user_hd.router)
 
 async def main():
     init_users_db()
-    
+
     await bot.set_my_commands([
         BotCommand(command="start", description="Запуск бота"),
     ])
 
-    await bot.delete_webhook(drop_pending_updates=True) 
-    
+    await bot.delete_webhook(drop_pending_updates=True)
+
     await dp.start_polling(bot)
 
-if __name__  == '__main__':
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     print('🤖 Бот включено та готово до роботи!')
-    
+
     try:
-        asyncio.run(main())  
+        asyncio.run(main())
     except KeyboardInterrupt:
         print('💤 Бот вимкнено!')
