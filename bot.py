17bfae2ba8a58c7d62d819a28ca40242a70d1b03
Jakkaru <jakkaru2010@gmail.com>
2026-07-21 08:28:17 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,5 +2,6 @@ from create_bot import dp
 from handlers import user_hd, admin_hd
 
 def register_all_handlers():
-    dp.include_router(user_hd.router)
     dp.include_router(admin_hd.router)
+    dp.include_router(user_hd.router)
+    
